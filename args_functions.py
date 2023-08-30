0be954998b6c4b3ad1a53d684dfb15907da38745
Rylan Cook <rcgamer2009@hotmail.com>
2023-08-30 11:21:44 -0600

    Clean up the args

--- args_functions.py
+++ args_functions.py
@@ -15,20 +15,23 @@ import path_functions
 
 def get_args():
     parser = configargparse.ArgumentParser(description="Makes an HTML file to browse a scenarios Characters")
-    parser.add_argument("-c", "--my-config", is_config_file=True, help="config file path")
+    parser.add_argument("-c", "--my-config", is_config_file=True, help="Config file path")
     parser.add_argument(
         "-i",
-        dest="inputdir",
-        help="Give input directory to make HTML File for. In directory, there should be scenario.yml and a characters folder.",
+        "--inputdir",
+        help="Run the program in a different directory.",
         type=path_functions.dir_path,
         default=os.path.abspath(os.curdir),
     )
     parser.add_argument("-s", "--silent", help="Will not ask for input", action="store_true")
-    argroup = parser.add_argument_group("Bounds functions")
+    argroup = parser.add_argument_group(
+        "Bounds functions",
+        description='Outbox "real size" (Image size after maximum crop). It will highlight any file that has a different size than the most common, or all if the most_common is 1.',
+    )
     argroup.add_argument(
         "-b",
         dest="bounds",
-        help='Don\'t make html File. Outbox "real size" (Image size after maximum crop). It will highlight any file that has a different size than the most common, or all if the most_common is 1.',
+        help="Run the bounds function.",
         action="store_true",
     )
     argroup.add_argument(
@@ -57,30 +60,28 @@ def get_args():
         action="store_false",
     )
     # Output a JSON File instead of HTML
-    argroup = parser.add_argument_group("Separate files")
-    argroup.add_argument(
-        "--explained",
-        help='This command is just explaination text. It does not change anything. These commands produce a JS file that stores the data. It has to be a JS file or it wont load correctly. The JS file is a json file but with "var data =" added',
+    argroup = parser.add_argument_group(
+        "Separate files",
+        description="These commands deal with creating the character data in a different file than the HTML. This aim of these commands are to allow for modifications to the HTML file that wont need to be overwritten if a character changes.",
     )
     argdiff = argroup.add_mutually_exclusive_group()
     argdiff.add_argument(
         "-j",
-        "--json",
-        dest="onlyjson",
-        help="Instead of creating an HTML file, output the JS that the HTML uses to display. This is useful if you have made modifications to the HTML code and dont want to remake the HTML file.",
+        "--onlyjson",
+        help="Instead of creating an HTML file, output the character data in a different file.",
         action="store_true",
     )
     argdiff.add_argument(
         "-sp",
         "--splitfiles",
-        help="Creates an HTML and JS file instead of a single HTML file. The HTML file will work the same way, but the JSON will not be embedded.",
+        help="Creates an HTML and character data file instead of a single HTML file. The HTML file will work the same way, but the character data will not be embedded.",
         action="store_true",
     )
 
     argroup.add_argument(
         "-jn",
-        "--jsname",
-        help="Name for the JSON file created if ",
+        "--jsonname",
+        help="Name for the character data file if created.",
         default="characters.js",
     )
     # Normal args
@@ -101,14 +102,14 @@ def get_args():
         "-cj",
         "--convertjson",
         dest="json2yaml",
-        help="Skip HTML and instead convert JSON files to yaml. Will walk through the whole directory and convert any found. Requires YAML for this program to work",
+        help="Skip HTML and instead convert JSON files to YML. Will walk through the whole directory and convert any found.",
         action="store_true",
     )
     argroup.add_argument(
         "-op",
         "--outfitpriority",
         dest="outfitprio",
-        help=f"Change the priority in which outfits are chosen. Accepts a python list made of strings and tuples. A tuple means both are wayed the same, but first one found will chosen for it. Current order, from left to right, is: {path_functions.OUTFIT_PRIO}",
+        help=f"Change the priority in which outfits are chosen. Accepts a python list made of strings and tuples. A tuple means both are treated the same, but first one found will chosen for it. Current order, from left to right, is: {path_functions.OUTFIT_PRIO}",
         type=ast.literal_eval,
     )
 
@@ -123,26 +124,26 @@ def get_args():
     argroup.add_argument(
         "-tn",
         "--titlename",
-        help="Use given name as Title (On main page) instead of the one from scenario.yaml.",
+        help="Use the given text as the title on main page instead of the one from scenario.yml.",
     )
     argroup.add_argument(
         "-mhm",
         "--maxheightmultiplier",
-        help="Change the max face height multiplier. The bigger the number the more it will show of the outfit. Default is 1.07",
+        help="Change the max face height multiplier. The bigger the number, the more it will show of the outfit on the expression sheet. Default is 1.07",
         type=float,
         default=1.07,
     )
     argroup.add_argument(
         "-oh",
         "--outfitheight",
-        help="Change the height of the outfits that are displayed on the main page and top of the character page. Default is 200",
+        help="Change the height of the outfits that are displayed on the main page and top of the character page. Default is 200px",
         type=int,
         default=200,
     )
     argroup.add_argument(
         "-ah",
         "--accessoryheight",
-        help="Change the height of the accessory picker that is . Default is 400",
+        help="Change the height of the accessory picker that is displayed on the character page. Default is 400px",
         type=int,
         default=400,
     )
@@ -158,12 +159,12 @@ def setup_args(args):
         builtins.input2 = builtins.input
         builtins.input = lambda x: ""
     if args.json2yaml:
-        print("Attempting to convert all JSON to YAML.")
+        print("Attempting to convert all JSON to YML.")
         json2yaml.json2yaml(argparse.Namespace(input_dir=args.inputdir))
         sys.exit()
     yml_data: dict = {}
     if not os.path.exists(os.path.join(args.inputdir, "scenario.yml")) and not args.bounds:
-        print(f"Error: Scenario.yaml does not exist in '{args.inputdir}'.")
+        print(f"Error: Scenario.yml does not exist in '{args.inputdir}'.")
         response = input(
             "Would you like to convert all JSON files to YAML? (Y|y for yes, anything else to exit): ",
         )
@@ -182,9 +183,7 @@ def setup_args(args):
                 sys.exit(1)
 
         if "title" not in yml_data:
-            print("Error: Title Not found in YAML file.")
-            input("Press Enter to exit...")
-            sys.exit(1)
+            yml_data["title"] = ""
 
         if args.titlename:
             yml_data["title"] = args.titlename
